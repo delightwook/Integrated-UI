b6ece88c958a7be7f89f6fbbd980a1ed3c509290
delightwook91 <delightwook@naver.com>
2018-09-10 11:26:40 +0900

    Fixed

--- vitrage_dashboard/dashboard/static/dashboard/project/entities/actionlist/entities-actionlist.directive.js
+++ vitrage_dashboard/dashboard/static/dashboard/project/entities/actionlist/entities-actionlist.directive.js
@@ -26,12 +26,7 @@ function hzEntitiesActionlist() {
             scope.typeArray=[];
             if(scope.actionList.length > 0 ){
                 for(var i = 0; i<scope.actionList.length; i++){
-                    if(scope.actionList[i] != 'Apimonitor' && scope.actionList[i] != 'Monitoring' && scope.actionList[i] != 'Testing' && scope.actionList[i] != 'Checkpoint' && scope.actionList[i] != 'Mistral' && scope.actionList[i] != 'Rally' &&
-                        scope.actionList[i].toLowerCase().match(scope.actionItem.vitrage_type) !=null) {
                         scope.typeArray.push(scope.actionList[i]);
-                    }else if((scope.actionList[i] == 'Apimonitor' ||scope.actionList[i] == 'Monitoring' || scope.actionList[i] == 'Testing' || scope.actionList[i] == 'Checkpoint' || scope.actionList[i] == 'Mistral' || scope.actionList[i] == 'Rally')){
-                        scope.typeArray.push(scope.actionList[i]);
-                    }
                 }
                 if(scope.typeArray.length > 0){
                     scope.selectedAction = scope.typeArray[0];
@@ -50,32 +45,20 @@ function hzEntitiesActionlist() {
               return scope.showbutton;
           }
         };
+
         scope.getComp = function(selectedAction,button_type){
-            if (selectedAction  != 'Rally' && button_type != "Action"){
-                if ((selectedAction.toLowerCase().match(scope.actionItem.vitrage_type)) != null){
-                    return false;
-                }else{
-                    return true;
-                }
-            }else if(selectedAction != 'Apimonitor' && selectedAction != 'Monitoring' && selectedAction != 'Testing' && selectedAction != 'Checkpoint' && selectedAction != 'Mistral' && selectedAction  != 'Rally' && button_type == "Action"){
-                return true;
+            if (button_type != "Action"){
+                return false;
             }else{
                 return false;
             }
         };
 
         scope.onRunClick = function(action_type) {
-            if ( action_type == 'Mistral' || action_type  == 'Rally'|| action_type  == 'Checkpoint' || action_type  == 'Testing' || action_type == 'Monitoring' || action_type == 'Apimonitor'){
               scope.$emit('selectedAction',[action_type,scope.actionItem.vitrage_type]);
-            }
         };
         scope.onNewtab = function(selectedAction) {
-          if (selectedAction != 'Mistral' && selectedAction != 'Rally'){
-              scope.$emit('newTab',scope.actionItem.url[scope.actionItem.vitrage_type]);
-          }
-          else if(selectedAction != 'Mistral'){
               scope.$emit('newTab',selectedAction);
-          }
         };
     }
 
